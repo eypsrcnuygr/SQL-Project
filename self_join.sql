43c32220a21275e36fd7126c6eda51b5358e5b18
Eyüp Sercan UYGUR <sercanuygur@gmail.com>
2020-08-25 12:45:07 +0300

    Change format

--- self_join.sql
+++ self_join.sql
@@ -1,16 +1,19 @@
-SELECT COUNT(*) FROM stops
+SELECT COUNT(*)
+FROM stops
 
-SELECT id FROM stops
+SELECT id
+FROM stops
 WHERE name = 'Craiglockhart'
 
 SELECT stops.id, stops.name
 FROM route
-JOIN stops ON stops.id = route.stop
+  JOIN stops ON stops.id = route.stop
 WHERE num = 4
-AND company = 'LRT'
+  AND company = 'LRT'
 
 SELECT company, num, COUNT(*)
-FROM route WHERE stop=149 OR stop=53
+FROM route
+WHERE stop=149 OR stop=53
 GROUP BY company, num
 HAVING COUNT(*) > 1
 
@@ -18,7 +21,7 @@ SELECT a.company, a.num, a.stop, b.stop
 FROM route a JOIN route b ON
   (a.company=b.company AND a.num=b.num)
 WHERE a.stop=53
-AND b.stop = 149
+  AND b.stop = 149
 
 SELECT a.company, a.num, stopa.name, stopb.name
 FROM route a JOIN route b ON
@@ -26,13 +29,13 @@ FROM route a JOIN route b ON
   JOIN stops stopa ON (a.stop=stopa.id)
   JOIN stops stopb ON (b.stop=stopb.id)
 WHERE stopa.name='Craiglockhart'
-AND stopb.name = 'London Road'
+  AND stopb.name = 'London Road'
 
 SELECT DISTINCT a.company, a.num
 FROM route a JOIN route b ON
   (a.company=b.company AND a.num=b.num)
 WHERE a.stop = 115
-AND b.stop = 137
+  AND b.stop = 137
 
 SELECT a.company, a.num
 FROM route a JOIN route b ON
@@ -40,27 +43,27 @@ FROM route a JOIN route b ON
   JOIN stops stopa ON (a.stop=stopa.id)
   JOIN stops stopb ON (b.stop=stopb.id)
 WHERE stopa.name='Craiglockhart'
-AND stopb.name = 'Tollcross'
+  AND stopb.name = 'Tollcross'
 
 SELECT stopb.name, a.company, a.num
 FROM route a
-JOIN route b ON a.company = b.company AND a.num = b.num
-JOIN stops stopa ON (a.stop=stopa.id)
-JOIN stops stopb ON (b.stop=stopb.id)
+  JOIN route b ON a.company = b.company AND a.num = b.num
+  JOIN stops stopa ON (a.stop=stopa.id)
+  JOIN stops stopb ON (b.stop=stopb.id)
 WHERE stopa.name='Craiglockhart'
 
-SELECT rt1.num, rt1.company, st1.name, rt3.num, rt3.company 
+SELECT rt1.num, rt1.company, st1.name, rt3.num, rt3.company
 FROM route rt1
-JOIN route rt2 ON rt1.num = rt2.num AND rt1.company = rt2.company
-JOIN route rt3 ON rt2.stop = rt3.stop
-JOIN route rt4 ON rt3.num = rt4.num AND rt3.company = rt4.company
-JOIN stops st1 ON st1.id = rt2.stop
+  JOIN route rt2 ON rt1.num = rt2.num AND rt1.company = rt2.company
+  JOIN route rt3 ON rt2.stop = rt3.stop
+  JOIN route rt4 ON rt3.num = rt4.num AND rt3.company = rt4.company
+  JOIN stops st1 ON st1.id = rt2.stop
 WHERE rt1.stop = (SELECT stops.id
-                 FROM stops
-                 WHERE name = 'Craiglockhart')
-AND rt4.stop = (SELECT stops.id
-               FROM stops
-               WHERE name = 'Lochend')
+  FROM stops
+  WHERE name = 'Craiglockhart')
+  AND rt4.stop = (SELECT stops.id
+  FROM stops
+  WHERE name = 'Lochend')
 ORDER BY rt1.num, st1.name, rt3.num;
 
 
